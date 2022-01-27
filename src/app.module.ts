d8912d9d9dbd54c1d5e50034f4c468e6f06ee55b
Knijus <justinas.kniuksta@gmail.com>
2022-01-27 14:09:02 +0200

    deleted unnessesary files: app.controller.ts, app.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -18,7 +16,5 @@ import { AppService } from './app.service';
       synchronize: true,
     })
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
