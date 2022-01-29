a0d667f1756c00a0bf18e1c85f67938cb5658d68
Knijus <justinas.kniuksta@gmail.com>
2022-01-29 11:51:25 +0200

    Added service files and implemented GET holiday service logic

---
+++ src/holiday/service/holiday.service.ts
@@ -0,0 +1,65 @@
+import { HttpService } from '@nestjs/axios';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { HolidayEntity } from '../models/holiday.entity';
+import { HolidayInterface } from '../models/holiday.interface';
+
+@Injectable()
+export class HolidayService {
+
+	constructor(
+		@InjectRepository(HolidayEntity)
+		private holidayRepository: Repository<HolidayEntity>,
+		private httpService: HttpService) {}
+
+	async getCountryHoliday(countryCode: string, year: number): Promise<HolidayInterface> {
+		const countryCodeYear = countryCode + year;
+		const countryHoliday = await this.holidayRepository.findOne(countryCodeYear);
+
+		if (countryHoliday) {
+			console.log("db");
+			const holidaysByMonth = countryHoliday.month;
+			return holidaysByMonth;
+		}
+
+		const ResponseFromEnrico = await this.httpService
+		.get(`${process.env.ENRICO_SERVICE}/${process.env.RESPONSE_TYPE}/${process.env.ENRICO_VERSION}?action=${process.env.ACTION_GET_HOLIDAYS_FOR_YEAR}&year=${year}&country=${countryCode}&holidayType=public_holiday`)
+		.toPromise();
+
+		const HolidaysByMonths = {};
+
+		for (let x = 1; x<= 12; x++) {
+			HolidaysByMonths[x]= [];
+		}		
+
+		if (ResponseFromEnrico.data.length) {
+			ResponseFromEnrico.data.forEach((data) => {
+				const day = {
+					day: data.date.day,
+					month: data.date.month,
+					year: data.date.year,
+					dayOfWeek: data.date.dayOfWeek,
+					name: data.name,
+					holidayType: data.holidayType,
+				}
+				HolidaysByMonths[data.date.month].push(day);
+			});
+
+			const saveToPg = {id: countryCodeYear, month: HolidaysByMonths};
+
+			this.holidayRepository.save(saveToPg);
+			console.log("enrico");
+
+			return HolidaysByMonths	
+		} else {
+			throw new HttpException({
+				status: HttpStatus.NOT_FOUND,
+				error: ResponseFromEnrico.data.error
+			}, HttpStatus.NOT_FOUND);
+		}
+
+
+
+	}
+}
