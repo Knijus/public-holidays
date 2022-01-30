69635b2ce5c9a58fa34ef9155450d5fc40430819
Knijus <justinas.kniuksta@gmail.com>
2022-01-30 18:05:54 +0200

    Created GET dayStatus API

--- src/day-status/controller/day-status.controller.ts
+++ src/day-status/controller/day-status.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { DaysStatusInterface } from '../models/day-status.interface';
+import { DayStatusService } from '../service/day-status.service';
 
-@Controller('day-status')
-export class DayStatusController {}
+@Controller('dayStatus')
+export class DayStatusController {
+	constructor (private readonly holidayService: DayStatusService) {}
+
+	@Get()
+	async getDayStatus(@Query("country") countryCode, @Query("date") date): Promise<DaysStatusInterface> {
+		console.log ("input:", countryCode, date);
+		return await this.holidayService.getDayStatus(countryCode, date);
+	}
+
+}
--- src/day-status/day-status.module.ts
+++ src/day-status/day-status.module.ts
@@ -1,9 +1,18 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DaysEntity } from 'src/holiday/models/days.entity';
+import { HolidayService } from 'src/holiday/service/holiday.service';
 import { DayStatusController } from './controller/day-status.controller';
 import { DayStatusService } from './service/day-status.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([DaysEntity]),
+    HttpModule,
+    
+],
   controllers: [DayStatusController],
-  providers: [DayStatusService]
+  providers: [DayStatusService, HolidayService]
 })
 export class DayStatusModule {}
