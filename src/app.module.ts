3dd122ff320123116754c9a00c3d9570c68b8116
Knijus <justinas.kniuksta@gmail.com>
2022-02-07 15:02:40 +0200

    added 4 endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { MaxFreedayModule } from './maxFreeday/maxFreeday.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      // port: parseInt(<string>process.env.POSTGRES_PORT),
+      port: parseInt(<string>process.env.POSTGRES_PORT),
       // username: process.env.POSTGRES_USER,
       // password: process.env.POSTGRES_PASSWORD,
       // database: process.env.POSTGRES_DATABASE,
@@ -22,7 +22,7 @@ import { MaxFreedayModule } from './maxFreeday/maxFreeday.module';
           ? { rejectUnauthorized: false }
           : false,
       autoLoadEntities: true,
-      synchronize: true,
+      //synchronize: true,
     }),
     CountriesModule,
     HolidayModule,
