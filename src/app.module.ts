a5c087a920e49b09d199691449a3619eac863a09
Knijus <justinas.kniuksta@gmail.com>
2022-02-07 14:20:16 +0200

    added 4 endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,15 +12,15 @@ import { MaxFreedayModule } from './maxFreeday/maxFreeday.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      // url: process.env.DATABASE_URL,
-      // ssl:
-      //   process.env.NODE_ENV === 'production'
-      //     ? { rejectUnauthorized: false }
-      //     : false,
+      // port: parseInt(<string>process.env.POSTGRES_PORT),
+      // username: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      // database: process.env.POSTGRES_DATABASE,
+      url: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
       autoLoadEntities: true,
       synchronize: true,
     }),
